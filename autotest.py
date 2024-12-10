24a4175217bcf979398a1d4ca2596cc4dba709af
Kate <Katezador-ipt27@lll.kpi.ua>
2024-12-10 23:59:03 +0200

    add edits

--- autotest.py
+++ autotest.py
@@ -1,9 +1,15 @@
 import unittest
 from modprlab1 import decrypt
 class decrypttestcase(unittest.TestCase):
-    def test_decrypt(self):
+    def test_decrypt1(self):
         result = decrypt("uryyb jbeyq")
         self.assertEqual(result, "hello world")
+    def test_decrypt2(self):
+        result = decrypt("Uryyb Jbeyq")
+        self.assertEqual(result, "Hello World")
+    def test_decrypt3(self):
+        result = decrypt(" ")
+        self.assertEqual(result, " ")        
 
 if __name__ == "__main__":
     unittest.main()        
